ab2bfde0c808cdeba8e44fe12f0b7836a66b030f
tehmou <timo.tuominen@gmail.com>
2016-08-12 14:21:13 +0100

    Add a custom observable for the incoming messages

--- app/src/main/java/com/futurice/rxandroidchatexercise/MainActivity.java
+++ app/src/main/java/com/futurice/rxandroidchatexercise/MainActivity.java
@@ -10,16 +10,24 @@ import com.github.nkzawa.socketio.client.Socket;
 
 import java.net.URISyntaxException;
 
+import rx.Observable;
+import rx.Subscription;
+import rx.functions.Action1;
+import rx.subscriptions.BooleanSubscription;
+import rx.subscriptions.CompositeSubscription;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private Socket socket = null;
+    private CompositeSubscription subscription;
+    private Socket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        subscription = new CompositeSubscription();
 
         try {
             socket = IO.socket("http://10.0.2.2:3000");
@@ -28,22 +36,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (socket != null) {
+            Observable<String> messages = createMessageListener(socket);
+            subscription.add(messages.subscribe(
+                    msg -> Log.d(TAG, "chat message: " + msg)
+            ));
             socket.on("chat message", onNewMessage);
             socket.connect();
         }
     }
 
+    private static Observable<String> createMessageListener(final Socket socket) {
+        return Observable.create(subscriber -> {
+            final Emitter.Listener listener =
+                    args -> subscriber.onNext(args[0].toString());
+            socket.on("chat message", listener);
+            subscriber.add(BooleanSubscription.create(
+                    () -> {
+                        Log.d(TAG, "unsubscribe");
+                        socket.off("chat message", listener);
+                    }));
+        });
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        socket.disconnect();
-        socket.off("chat message", onNewMessage);
+        if (socket != null) {
+            socket.disconnect();
+            socket.off("chat message", onNewMessage);
+            socket = null;
+        }
+        if (subscription != null) {
+            subscription.clear();
+            subscription = null;
+        }
     }
 
-    private Emitter.Listener onNewMessage = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            Log.d(TAG, "chat message: " + args[0]);
-        }
-    };
+    private Emitter.Listener onNewMessage = args -> Log.d(TAG, "chat message: " + args[0]);
 }
